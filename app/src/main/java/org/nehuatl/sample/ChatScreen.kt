bf8c8ecc28cbe7777416179218ee9b892fefb419
ztrosenkova-lang <ztrosenkova@gmail.com>
2026-07-11 09:32:07 +0300

    Update ChatScreen.kt

--- app/src/main/java/org/nehuatl/sample/ChatScreen.kt
+++ app/src/main/java/org/nehuatl/sample/ChatScreen.kt
@@ -87,7 +87,7 @@ fun ChatScreen(
             onLoad = {
                 showModelDialog = false
                 if (currentModelPath != null) {
-                    viewModel.loadModel(currentModelPath, mmprojPath)
+                    viewModel.loadModel(currentModelPath,)
                 }
             },
             onDismiss = if (currentModelPath != null) {
