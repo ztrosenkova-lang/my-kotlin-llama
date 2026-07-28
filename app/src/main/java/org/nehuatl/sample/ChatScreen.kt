4133c40ef7b9b1a7e1718ba685ac7f2df2e99686
ztrosenkova-lang <ztrosenkova@gmail.com>
2026-07-28 13:50:17 +0300

    Update ChatScreen.kt

--- app/src/main/java/org/nehuatl/sample/ChatScreen.kt
+++ app/src/main/java/org/nehuatl/sample/ChatScreen.kt
@@ -2,6 +2,7 @@ package org.nehuatl.sample
 
 import android.Manifest
 import android.content.pm.PackageManager
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.animation.core.LinearOutSlowInEasing
 import androidx.compose.animation.core.tween
