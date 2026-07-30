4bec5ca43c89772cd5b547b5f79ff53f54501a66
ztrosenkova-lang <ztrosenkova@gmail.com>
2026-07-30 12:24:44 +0300

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,7 +12,6 @@ dependencyResolutionManagement {
         google()
         mavenCentral()
         maven { url = java.net.URI("https://jitpack.io") }
-        // Репозиторий для sherpa-onnx (Maven Central уже содержит нужный артефакт)
     }
 }
 
