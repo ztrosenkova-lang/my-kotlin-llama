ca7ee78f7ba3020f13465b704f0761edcdd9c263
ztrosenkova-lang <ztrosenkova@gmail.com>
2026-08-25 22:57:09 +0300

    Update TtsService.kt

--- app/src/main/java/org/nehuatl/sample/TtsService.kt
+++ app/src/main/java/org/nehuatl/sample/TtsService.kt
@@ -106,7 +106,7 @@ class TtsService : Service() {
                     val dataDir = copyDataDir("tts-model/espeak-ng-data")
 
                     val modelConfig = OfflineTtsVitsModelConfig(
-                        model = "tts-model/model.onnx",
+                        model = "tts-model/ru_RU-ruslan-medium.onnx",
                         tokens = "tts-model/tokens.txt",
                         dataDir = dataDir,
                         noiseScale = 0.35f,
