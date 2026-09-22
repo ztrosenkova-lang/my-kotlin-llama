5eea57363e69e89a3ad6bb80ed583318441318ce
ztrosenkova-lang <ztrosenkova@gmail.com>
2026-09-22 11:36:17 +0300

    Create ComposeOverlayView.kt

---
+++ app/src/main/java/org/nehuatl/sample/ComposeOverlayView.kt
@@ -0,0 +1,49 @@
+package org.nehuatl.sample
+
+import android.content.Context
+import androidx.compose.ui.platform.ComposeView
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LifecycleRegistry
+import androidx.lifecycle.ViewModelStore
+import androidx.lifecycle.ViewModelStoreOwner
+import androidx.lifecycle.setViewTreeLifecycleOwner
+import androidx.lifecycle.setViewTreeViewModelStoreOwner
+import androidx.savedstate.SavedStateRegistry
+import androidx.savedstate.SavedStateRegistryController
+import androidx.savedstate.SavedStateRegistryOwner
+import androidx.savedstate.setViewTreeSavedStateRegistryOwner
+
+class ComposeOverlayView(context: Context) :
+    ComposeView(context),
+    LifecycleOwner,
+    ViewModelStoreOwner,
+    SavedStateRegistryOwner {
+
+    private val lifecycleRegistry = LifecycleRegistry(this)
+    private val savedStateRegistryController = SavedStateRegistryController.create(this)
+
+    override val lifecycle: Lifecycle get() = lifecycleRegistry
+    override val viewModelStore: ViewModelStore = ViewModelStore()
+    override val savedStateRegistry: SavedStateRegistry
+        get() = savedStateRegistryController.savedStateRegistry
+
+    init {
+        setViewTreeLifecycleOwner(this)
+        setViewTreeViewModelStoreOwner(this)
+        setViewTreeSavedStateRegistryOwner(this)
+
+        savedStateRegistryController.performRestore(null)
+        lifecycleRegistry.currentState = Lifecycle.State.RESUMED
+    }
+
+    override fun onAttachedToWindow() {
+        super.onAttachedToWindow()
+        lifecycleRegistry.currentState = Lifecycle.State.RESUMED
+    }
+
+    override fun onDetachedFromWindow() {
+        lifecycleRegistry.currentState = Lifecycle.State.DESTROYED
+        super.onDetachedFromWindow()
+    }
+}
