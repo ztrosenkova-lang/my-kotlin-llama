bd372169a6d1966d94da0366ba026c614ad1d123
ztrosenkova-lang <ztrosenkova@gmail.com>
2026-09-23 10:21:24 +0300

    Update RobotOverlayContent.kt

--- app/src/main/java/org/nehuatl/sample/RobotOverlayContent.kt
+++ app/src/main/java/org/nehuatl/sample/RobotOverlayContent.kt
@@ -1,6 +1,7 @@
 package org.nehuatl.sample
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
